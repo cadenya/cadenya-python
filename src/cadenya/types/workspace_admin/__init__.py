3ec873b48d22a03d39783f63acfd3f55a02bd84f
Robert Ross <me@bobbytables.io>
2026-08-14 23:07:46 -0400

    Sync from cadenya-python-staging (#10)

--- src/cadenya/types/workspace_admin/__init__.py
+++
@@ -1,7 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from __future__ import annotations
-
-from .member_add_params import MemberAddParams as MemberAddParams
-from .member_list_params import MemberListParams as MemberListParams
-from .profile_list_params import ProfileListParams as ProfileListParams
